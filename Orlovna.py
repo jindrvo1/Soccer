9793b7ad408f3a1030a15f5d74a2a80c34a910c6
Vojtech Jindra <jindravo@gmail.com>
2017-11-26 12:25:05 +0100

    added data

--- Orlovna.py
+++ Orlovna.py
@@ -2,11 +2,16 @@ import Soccer as s
 
 s.eval_match(['Vojta1', 'Vojta2', 'Krystof'], ['Vdolek', 'Marta', 'Vanan'], 10, 8)
 s.eval_match(['Marta', 'Vojta1', 'Krystof'], ['Vdolek', 'Vojta2', 'Vanan'], 2, 5)
+
 s.eval_match(['Marek', 'Vojta2', 'Vdolek'], ['Vojta1', 'Vanan', 'Marta'], 11, 13)
 s.eval_match(['Marta', 'Vojta2', 'Marek'], ['Vdolek', 'Vojta1', 'Vanan'], 6, 2)
+
 s.eval_match(['Krystof', 'Panek', 'Vojta2'], ['Vanan', 'Klizka', 'Marek'], 10, 2)
 s.eval_match(['Panek', 'Klizka', 'Vojta2'], ['Vanan', 'Krystof', 'Marek'], 10, 4)
 s.eval_match(['Krystof', 'Vanan', 'Vojta2'], ['Klizka', 'Panek', 'Marek'], 10, 5)
+
+s.eval_match(['Krystof', '-_-', 'Marta'], ['Vojta2', 'Vojta1', 'Vdolek'], 11, 9)
+s.eval_match(['Krystof', '-_-', 'Vdolek'], ['Vojta2', 'Vojta1', 'Marta'], 9, 11)
 	
 s.print_ladders()
 print()
